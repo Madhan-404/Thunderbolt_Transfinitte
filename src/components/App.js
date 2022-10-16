4ee569c9c46a0299d2adaf26d267afcd30a5c9ec
madhan <madhansainik@gmail.com>
2022-10-16 09:59:06 +0530

    fixed minting issues

---
+++ src/components/App.js
@@ -0,0 +1,82 @@
+import React,{ useState, useEffect } from 'react';
+import Header from './sections/Header';
+import { useDispatch } from 'react-redux';
+import { TezosToolkit} from "@taquito/taquito";
+import { BeaconWallet } from "@taquito/beacon-wallet";
+import {
+  NetworkType,
+  BeaconEvent,
+  defaultEventCallbacks,
+  ColorMode
+} from "@airgap/beacon-sdk";
+
+import { fetchContractData, fetchData, _walletConfig } from '../actions';
+import { Route, Routes } from 'react-router-dom';
+import Home from './layouts/Home';
+import Create from './layouts/Create';
+import Show from './layouts/Show';
+
+
+const App = () => {
+    const dispatch = useDispatch();
+    const [Tezos, setTezos] = useState(
+        new TezosToolkit("https://ghostnet.smartpy.io")
+    );
+    const [wallet, setWallet] = useState(null);
+
+    useEffect(()=>{
+        (async () => {
+            const wallet_instance = new BeaconWallet({
+                name: "NFT marketplace",
+                preferredNetwork: NetworkType.CUSTOM,
+                colorMode: ColorMode.LIGHT,
+                disableDefaultEvents: false, // Disable all events / UI. This also disables the pairing alert.
+                eventHandlers: {
+                // To keep the pairing alert, we have to add the following default event handlers back
+                [BeaconEvent.PAIR_INIT]: {
+                    handler: defaultEventCallbacks.PAIR_INIT
+                },
+                [BeaconEvent.PAIR_SUCCESS]: {
+                    handler: data => { return (data.publicKey);}
+                }
+                }
+            });
+            Tezos.setWalletProvider(wallet_instance);
+            const activeAccount = await wallet_instance.client.getActiveAccount();
+            if (activeAccount) {
+                const userAddress = await wallet_instance.getPKH();
+                const balance = await Tezos.tz.getBalance(userAddress);
+                dispatch(_walletConfig(
+                    {
+                        userAddress: userAddress, 
+                        balance: balance.toNumber()
+                    }));
+            }
+            setWallet(wallet_instance);
+        })();
+    },[Tezos, dispatch]);
+
+
+    useEffect(()=>{
+        dispatch(fetchContractData({Tezos}));
+    },[Tezos, dispatch]);
+
+    useEffect(()=>{
+        dispatch(fetchData());
+    },[dispatch]);
+
+    return (
+        <div className="ui container">
+        <Header Tezos={Tezos} setTezos={setTezos} wallet={wallet} />
+        <div className="ui container">
+            <Routes>
+                <Route path="/create" element={<Create Tezos={Tezos}/>}/>
+                <Route path="/show/:id" element={<Show Tezos={Tezos}/>}/>
+                <Route path="/" element={<Home Tezos={Tezos}/>}/>
+            </Routes>
+        </div>
+    </div>
+    );
+}
+
+export default App;
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const config = {
+        contractAddress : "KT1LhYWsaqUo9EVmbgpZnXghQD5sk7qDhvmx", 
+        tokenAddress: "KT1RjETDWqyKseaZf5PF9L8kMGiv9nzVyaVo"
+}
+
+export default config;
